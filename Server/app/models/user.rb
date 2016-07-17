c692c56751617b33a70f8d197af8615470d392d8
maxpleaner <maxpleaner@gmail.com>
2016-07-17 12:40:53 -0700

    some user model stuff

--- Server/app/models/user.rb
+++ Server/app/models/user.rb
@@ -1,2 +1,19 @@
+require 'secure_random'
+
 class User < ApplicationRecord
+  validates :password_digest, presence: true
+  validates :name,            presence: true
+  has_secure_password
+
+  def name=(val)
+    super(val.blank? ? nil : val)
+  end
+
+  def login(password)
+    update(session_token: SecureRandom.urlsafe_base64)
+  end
+
+  def logout
+    update(session_token: nil)    
+  end
 end
